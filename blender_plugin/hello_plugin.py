775abf5a24f5741a3e121ed2884963f6e74e7947
thelazyone <giacomo.pantalone@gmail.com>
2024-05-26 22:12:57 +0200

    Updated the plugin to display a button in a "TBR" tab.

--- blender_plugin/hello_plugin.py
+++ blender_plugin/hello_plugin.py
@@ -1,8 +1,6 @@
 import bpy
 import os
 
-# Test plugin to use as a blueprint for future compiled code in blender.
-
 bl_info = {
     "name": "Hello Blender Plugin",
     "blender": (4, 0, 0),
@@ -10,8 +8,8 @@ bl_info = {
     "description": "Rust wrapping in blender test",
     "author": "Your Name",
     "version": (0, 1, 0),
-    "location": "View3D > Tool Shelf",
-    "warning": "",  # Used for warning icon and text in addons panel
+    "location": "View3D > Tool Shelf > TBR Panel",
+    "warning": "",
     "support": "COMMUNITY"
 }
 
@@ -25,22 +23,37 @@ except ImportError as e:
     print("DEBUG: Failed to import hello_plugin_lib:", e)
     raise
 
+# Trivial operator, just calling the hello_blender Rust function.
 class TBRPluginOperator(bpy.types.Operator):
     """TBR Plugin Description"""
     bl_idname = "wm.tbr_plugin"
     bl_label = "TBR Plugin Operator"
 
     def execute(self, context):
-        message = "hi" #hello_blender()
+        message = hello_blender()
         self.report({'INFO'}, message)
         return {'FINISHED'}
 
+# Simple pannel with one button.
+class TBR_PT_Panel(bpy.types.Panel):
+    """Creates a Panel in the Object properties window"""
+    bl_label = "TBR Plugin Panel"
+    bl_idname = "TBR_PT_Panel"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'TBR'
+
+    def draw(self, context):
+        layout = self.layout
+        layout.operator(TBRPluginOperator.bl_idname, text="Run TBR Plugin")
+
 def register():
-    print("Registering TBR Plugin...")
     bpy.utils.register_class(TBRPluginOperator)
+    bpy.utils.register_class(TBR_PT_Panel)
 
 def unregister():
     bpy.utils.unregister_class(TBRPluginOperator)
+    bpy.utils.unregister_class(TBR_PT_Panel)
 
 if __name__ == "__main__":
     register()
